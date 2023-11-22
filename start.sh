2af518261c112c7ce4015280ded0445f1a18c9a8
SURI <142067961+ImSuri919@users.noreply.github.com>
2023-11-22 11:31:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/azanhelpdesk/DQ-_TOM.git /DQ-_TOM
+  git clone https://github.com/ImSuri919/DQ-Tom-.git /DQ-Tom-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-_TOM
+  git clone $UPSTREAM_REPO /DQ-Tom-
 fi
-cd /DQ-_TOM
+cd /DQ-Tom-
 pip3 install -U -r requirements.txt
-echo "Starting DQ-_TOM...."
+echo "Starting DQ-Tom-...."
 python3 bot.py
